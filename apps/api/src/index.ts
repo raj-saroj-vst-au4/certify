fbd9a48bc5822f0a98c34e1bf57266b44732e580
Turbobot <turbobot@vercel.com>
2023-11-13 15:24:17 +0530

    feat(create-turbo): create kitchen-sink

---
+++ apps/api/src/index.ts
@@ -0,0 +1,9 @@
+import { log } from "logger";
+import { createServer } from "./server";
+
+const port = process.env.PORT || 5001;
+const server = createServer();
+
+server.listen(port, () => {
+  log(`api running on ${port}`);
+});
