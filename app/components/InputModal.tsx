01a9e5d309b8de5aa7f8ee159c71fe01cad58f07
Raj Saroj <54412653+raj-saroj-vst-au4@users.noreply.github.com>
2024-03-24 18:59:44 +0530

    Update InputModal.tsx
    Fixed input type number did not suffice

--- app/components/InputModal.tsx
+++ app/components/InputModal.tsx
@@ -61,8 +61,14 @@ const InputModal = ({
                     Your Certificate ID
                   </label>
                   <input
-                    type="number"
-                    onChange={(e) => setCertid(parseInt(e.target.value))}
+                    onKeyPress={(event) => {
+        if (!/[0-9]/.test(event.key)) {
+          event.preventDefault();
+        }else {
+          setCertid(parseInt(event.target.value))
+        }
+      }}
+                    
                     placeholder="6-Digit code"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                     required
