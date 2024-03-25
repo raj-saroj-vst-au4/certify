6976a932c5afca70e1d6031ff1a2ad68cfe14749
Raj Saroj <54412653+raj-saroj-vst-au4@users.noreply.github.com>
2024-03-25 14:37:23 +0530

    Update InputModal.tsx

--- app/components/InputModal.tsx
+++ app/components/InputModal.tsx
@@ -93,7 +93,7 @@ const InputModal = ({
                       Last 4-digits of Phone
                     </label>
                     <input
-                      type="number"
+                      type="tel"
                       id="phone"
                       onChange={(e) => setPhoneDigits(parseInt(e.target.value))}
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
